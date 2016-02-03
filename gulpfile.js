d9481f5f6a6a55c5b9574e868d73dc0c727b1469
Mohamed Said <themohamedsaid@gmail.com>
2016-02-03 10:01:26 +0000

    sass files separation

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var elixir = require('laravel-elixir');
 elixir(function (mix) {
     mix
         .styles('*.css', 'public/css/libs.css')
+
         .sass('app.sass')
+
         .browserSync({
             proxy: 'kanban.app'
         })
